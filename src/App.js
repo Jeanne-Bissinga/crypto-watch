16b9036b60b633bda28d4e44abd97cdf1aa20946
Jeanne-Bissinga <bissingajeanyne@gmail.com>
2025-03-04 21:04:28 +0100

    Mise en place du header

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderInfos from "./components/HeaderInfos";
+
+const App = () => {
+  return (
+    <div>
+      <div className="app-container">
+        <header>
+          <HeaderInfos />
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default App;
